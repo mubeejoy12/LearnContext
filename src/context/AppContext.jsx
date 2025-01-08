a51b4c331ec6dff3815303f51960c5d1a4d19195
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2025-01-08 17:21:12 +0100

    type of state function that change between light and dark

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,3 +1,9 @@
 import { createContext, useContext, useReducer } from "react";
 const AppContext = createContext();
 const initialState = { theme: "light", user: null };
+  switch (action.type) {
+    case "toggle_theme":
+      return {
+        ...state,
+        theme: state.theme === "light" ? "dark" : "light",
+      };
