211d6eafdb49dff9d7d22a0c2b6244e6fe370c80
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2025-01-08 10:36:01 +0100

    refactored context setup: uncomment all codes

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,16 +1,16 @@
-import { createContext, useContext, useState } from "react";
+// import { createContext, useContext, useState } from "react";
 
-const AuthContext = createContext();
-export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
+// const AuthContext = createContext();
+// export const AuthProvider = ({ children }) => {
+//   const [user, setUser] = useState(null);
 
-  const login = (name, email) => setUser({ name, email });
-  const logout = () => setUser(null);
+//   const login = (name, email) => setUser({ name, email });
+//   const logout = () => setUser(null);
 
-  return (
-    <AuthContext.Provider value={{ user, login, logout }}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
-export const useAuth = () => useContext(AuthContext);
+//   return (
+//     <AuthContext.Provider value={{ user, login, logout }}>
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// };
+// export const useAuth = () => useContext(AuthContext);
